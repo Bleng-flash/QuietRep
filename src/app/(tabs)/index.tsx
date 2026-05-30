d9a73eadbb40e5d26a863a9f5bceb006e4ca2dc1
Bryan Leng <bryan.leng33@gmail.com>
2026-05-30 14:51:45 +0800

    Set project directory structure and tabs

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function HomeScreen() {
   return (
     <View
       style={{
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={{ color: "red" }}>Dis is the home screen.</Text>
     </View>
   );
 }
